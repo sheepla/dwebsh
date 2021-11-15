92698d87b3bd73c8cd4e8d5c852b0e7b7747d264
Sheepla <62412884+sheepla@users.noreply.github.com>
2021-11-15 20:26:20 +0900

    [fix] cliffyを使うように変更

--- deps.ts
+++ deps.ts
@@ -1,9 +1,2 @@
-import {
-  Command,
-  ValidationError,
-} from "https://deno.land/x/cliffy@v0.19.3/command/mod.ts";
-
-import { version } from "./version.ts";
-const commandWithVersion = new Command().version(version);
-
-export { commandWithVersion, ValidationError };
+import { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
+export { Command };
