2c20378236a3715e22e508b68980d31bf32bd64f
Sheepla <62412884+sheepla@users.noreply.github.com>
2021-11-15 20:47:22 +0900

    [fix] 不要なコメントアウトを除去

--- mod.ts
+++ mod.ts
@@ -1,9 +1,3 @@
-// async function ping() {
-//   const pingUrl = baseUrl + "/ping";
-//   const res = await fetch(pingUrl, { method: "GET" });
-//   return res.json();
-// }
-
 export async function post(code: string) {
   const baseUrl = "https://websh.jiro4989.com/api";
   const postUrl = baseUrl + "/shellgei";
@@ -17,9 +11,3 @@ export async function post(code: string) {
   });
   return res.json();
 }
-
-// const res = await ping();
-// console.log(res);
-//const res = await post(Deno.args[0]);
-//console.log(res["stdout"]);
-//console.log(res["stderr"]);
